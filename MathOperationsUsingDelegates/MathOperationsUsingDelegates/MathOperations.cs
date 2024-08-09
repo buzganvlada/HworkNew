d1e79ebad6c2c372f159d2c3eacebc0f4361a13d
axelpxl33 <akselnordevpxl33@gmail.com>
2024-08-09 15:44:50 +0300

    Add project files.

---
+++ MathOperationsUsingDelegates/MathOperationsUsingDelegates/MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationsUsingDelegates
+{
+        public delegate int MathOperation(int firstNum, int secondNum);
+
+    public class Operation
+    {
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public int Subtract(int firstNum, int secondNum)
+        {
+            return secondNum - firstNum;
+        }
+        public int Multiply(int firstNum, int secondNum)
+        {
+            return firstNum * secondNum;
+        }
+        public int Divide(int firstNum, int secondNum)
+        {
+            return firstNum / secondNum;    
+        }
+    }
+}
